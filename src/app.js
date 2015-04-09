e49c06b665224fcb29b26f78add294a84d5eabb3
John Swartzentruber <github@mcswartz.org>
2015-04-09 09:22:54 -0400

    Ready for Config
    Still has a problem that requires phone app to be started sometimes. Probably am crashing.

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ var config = {
 
 // Set up the model/viewmodel
 var model = {
-    startStation: 'Swarthmore',
-    destStation: 'Suburban Station',
+    startStation: '',
+    destStation: '',
     trains: []
 };
 
@@ -36,7 +36,9 @@ function parseResult(train) {
 }
 
 function getNextToArrive() {
-    var url = encodeURI('http://www3.septa.org/hackathon/NextToArrive/' + model.startStation + '/' + model.destStation + '/5');
+    var url = encodeURI('http://www3.septa.org/hackathon/NextToArrive/' + model.startStation + '/' + model.destStation + '/4');
+    
+    refreshView();
     ajax({
         url: url,
         type: 'json'
@@ -54,54 +56,51 @@ function getNextToArrive() {
     });
 }
 
-function swap() {
-    console.log('Swapping' + model.startStation + ' with ' + model.destStation);
-    var s = model.startStation;
-    model.startStation = model.destStation;
-    model.destStation = s;
+function swapDest() {
+    if (model.destStation !== config.home && model.startStation !== config.home) {
+        model.destStation = config.home;
+    } else if (model.destStation !== config.work && model.startStation !== config.work) {
+        model.destStation = config.work;
+    } else {
+        var temp = model.destStation;
+        model.destStation = model.startStation;
+        model.startStation = temp;
+    }
+    
     model.trains = [];
-    refreshView();
     getNextToArrive();
 }
 
 function setStartStation(station) {
-    if (station !== model.startStation) {
+    if (station && station !== model.startStation) {
         model.trains = [];
         model.startStation = station;
-        if (model.startStation === config.home) {
-            model.destStation = config.work;
-        } else {
-            model.destStation = config.home;
-        }
-        refreshView();
+        model.destStation = model.startStation === config.home ? config.work : config.home;
+    
         getNextToArrive();
     }
 }
 
-refreshView();
-View.window.show();
-View.window.on('click', 'up', swap);
-View.window.on('click', 'down', swap);
+// Set the default stations before doing geolocation
+setStartStation(config.home);
 
-getNextToArrive();
+View.window.show();
+View.window.on('click', 'up', swapDest);
+View.window.on('click', 'down', swapDest);
 
 // Update every minute
 setInterval(function() { getNextToArrive(); }, 60000);
 
 // Geolocation processing
 (function () {
-    var prevLat = 400, prevLong = 400;
-    var resolution = 0.002;
-    
-    navigator.geolocation.watchPosition(
+    var watchId = navigator.geolocation.watchPosition(
         function (pos) {
-            var diffLat = Math.abs(prevLat - pos.coords.latitude);
-            var diffLong = Math.abs(prevLong - pos.coords.longitude);
-            if (diffLat > resolution || diffLong > resolution) {
-                prevLat = pos.coords.latitude;
-                prevLong = pos.coords.longitude;
-                var station = stations.closestStation(prevLat, prevLong);
-                setStartStation(station);
+            console.log('accuracy: ', pos.coords.accuracy, 'coords: ' + pos.coords.latitude + '/' + pos.coords.longitude);
+            var station = stations.closestStation(pos.coords.latitude, pos.coords.longitude);
+            setStartStation(station);
+            
+            if (station && pos.coords.accuracy <= 50) {
+                navigator.geolocation.clearWatch(watchId);
             }
         }, 
         function (err) {
@@ -110,6 +109,21 @@ setInterval(function() { getNextToArrive(); }, 60000);
         {
             enableHighAccuracy: false,
             maximumAge: 150000,
-            timeout: 10000
+            timeout: 15000
         });
 })();
+
+Pebble.addEventListener('showConfiguration', function() {
+    console.log('!!Settings requested!');
+    Pebble.openURL('http://pebble.johnswartzentruber.com/Septa.html');
+});
+    
+Pebble.addEventListener("webviewclosed", function(e) {
+    console.log("configuration closed");
+    
+    // webview closed
+    if (e.response) {
+        var options = JSON.parse(decodeURIComponent(e.response));
+        console.log("Options = " + JSON.stringify(options));
+    }
+}); 
--- src/view.js
+++ src/view.js
@@ -6,31 +6,12 @@ var window = new UI.Window();
 
 // 144x168
 
-// Create TimeText
-var timeText = new UI.TimeText({
-  position: new Vector2(0, 0),
-  size: new Vector2(144, 22),
-  text: "%I:%M",
-  font: 'Gothic-18-Bold',
-  color: 'white',
-  textAlign: 'center'
-});
-
-
-// Create a background Rect
-var stationRect = new UI.Rect({
-  position: new Vector2(0, 0),
-  size: new Vector2(144, 24),
-  backgroundColor: 'white'
-});
-
-
 // Create the Station Text
 var stationText = new UI.Text({
     position: new Vector2(0, -4),
     size: new Vector2(144, 24),
     font: 'Gothic-24-bold',
-    color: 'black',
+    color: 'white',
     text: '',
     textAlign: 'center',
     textOverflow: 'fill'
@@ -38,26 +19,26 @@ var stationText = new UI.Text({
 
 // Create TimeText
 var timeText = new UI.TimeText({
-    position: new Vector2(0, 20),
-    size: new Vector2(144, 22),
-    text: "%H:%M",
-    font: 'Gothic-24-Bold',
+    position: new Vector2(0, 18),
+    size: new Vector2(144, 26),
+    text: "%I:%M",
+    font: 'Bitham-30-black',
     color: 'white',
     textAlign: 'center'
 });
 
 
-// Create a background Rect
+// Create a trains background Rect
 var trainsRect = new UI.Rect({
-  position: new Vector2(0, 50),
-  size: new Vector2(144, 122),
+  position: new Vector2(0, 55),
+  size: new Vector2(144, 114),
   backgroundColor: 'white'
 });
 
 var trains = [];
 trains.push(new UI.Text({
-    position: new Vector2(2, 50),
-    size: new Vector2(142, 35),
+    position: new Vector2(2, 54),
+    size: new Vector2(142, 33),
     font: 'Gothic-28-Bold',
     color: 'black',
     text: '',
@@ -65,8 +46,8 @@ trains.push(new UI.Text({
 }));
 
 trains.push(new UI.Text({
-    position: new Vector2(2, 85),
-    size: new Vector2(142, 35),
+    position: new Vector2(2, 87),
+    size: new Vector2(142, 33),
     font: 'Gothic-28',
     color: 'black',
     text: '',
@@ -75,7 +56,7 @@ trains.push(new UI.Text({
 
 trains.push(new UI.Text({
     position: new Vector2(2, 120),
-    size: new Vector2(142, 35),
+    size: new Vector2(142, 33),
     font: 'Gothic-28',
     color: 'black',
     text: '',
@@ -84,7 +65,6 @@ trains.push(new UI.Text({
 
 // Show the Window
 window.add(timeText);
-window.add(stationRect);
 window.add(stationText);
 window.add(trainsRect);
 
